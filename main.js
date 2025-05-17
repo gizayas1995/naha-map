75a50f1d801cf6f199c7c39a6e521ce734b3de06
gizayas1995 <ayanosatobluegreen95@gmail.com>
2025-05-18 07:25:31 +0900

    Add files via upload

--- main.js
+++ main.js
@@ -1,9 +1,26 @@
 
-const map = L.map('map').setView([26.2122, 127.7129], 15);
+const map = L.map('map').setView([26.2119, 127.7127], 15);
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
   attribution: '© OpenStreetMap contributors'
 }).addTo(map);
 
-// Spot example
-L.marker([26.2122, 127.7129]).addTo(map)
-  .bindPopup('ハンタガー水源').openPopup();
+// Character selection
+let selectedCharacter = null;
+
+function selectCharacter(name) {
+  selectedCharacter = name;
+  document.getElementById("character-modal").style.display = "none";
+
+  const icon = L.icon({
+    iconUrl: 'img/' + name + '.png',
+    iconSize: [48, 48],
+    iconAnchor: [24, 24]
+  });
+
+  L.marker([26.2119, 127.7127], { icon: icon }).addTo(map)
+    .bindPopup("ここがハンタガー！");
+}
+
+window.onload = () => {
+  document.getElementById("character-modal").style.display = "flex";
+}
